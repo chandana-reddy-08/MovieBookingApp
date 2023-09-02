6da383936eb038ea7e4722b9898143ed9de7261b
Chandana G A <117585583+chandana-reddy-08@users.noreply.github.com>
2023-09-02 10:27:08 +0530

    Add files via upload

---
+++ FinalProjectMovieApp/Final Project/movieBooking/app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //url: "mongodb://localhost:27017/moviesdb" if this is not connected then you should explicitly specify the IPv4 loopback address (127.0.0.1) as below
+    url: "mongodb://127.0.0.1:27017/moviesdb"
+  };
